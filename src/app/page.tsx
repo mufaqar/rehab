c12bc2fd64b64e80e865d2f517593c2d80a5076b
mufaqar <mufaqar@gmail.com>
2023-10-29 17:46:33 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-evenly p-24 bg-white">
-
-
-      <img src="/images/logo.svg" alt="Rehab Fit" width={450} height={650} alt="Rehab Fit" />
-      <img src="/coming.jpg" alt="Coming Soon" />
-
+      <Image src="/images/logo.svg" alt="Rehab Fit" width={450} height={650} />
+      <Image src="/coming.jpg" alt="Coming Soon" width={624} height={312} />
     </main>
   )
 }
